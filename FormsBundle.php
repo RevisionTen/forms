bb2e459cb2d7581426e0556c25bbf14e4fffcc69
Cornelius K <kahns@bdrops.de>
2019-08-13 11:11:19 +0200

    Fixed onValidate

--- FormsBundle.php
+++ FormsBundle.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class FormsBundle extends Bundle
 {
-    public const VERSION = '1.1.5';
+    public const VERSION = '1.1.6';
 
     /**
      * {@inheritdoc}
