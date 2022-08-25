f10187d2b59a8c4b58eb83dc507b4701c50b16da
Cornelius Kahns <67231872+TAR5@users.noreply.github.com>
2022-08-25 16:53:01 +0200

    Fixed FormRead relationship class name in FormSubmission

--- FormsBundle.php
+++ FormsBundle.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FormsBundle extends Bundle
 {
-    public const VERSION = '3.0.6';
+    public const VERSION = '3.0.7';
 }
 
 class_alias(FormRead::class, '\\RevisionTen\\Forms\\Model\\FormRead');
